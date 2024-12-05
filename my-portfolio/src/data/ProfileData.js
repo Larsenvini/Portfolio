73144071104487d496fc62ced0eef7b156cb0042
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-12-04 23:43:20 -0300

    projectsupdated

--- my-portfolio/src/data/ProfileData.js
+++ my-portfolio/src/data/ProfileData.js
@@ -3,7 +3,7 @@ const profileData = {
     title: "Software Engineer",
     email: "larsenvini8@gmail.com",
     location: "Rio de Janeiro, Brazil.",
-    aboutMe: "Passionate software engineer with expertise in full-stack development and Data Science/AI. I love creating efficient and scalable web applications, along with working with data and AI to solve real-world problems ",
+    aboutMe: "Passionate software engineer with expertise in full-stack development and Data Science/AI. I love creating efficient and scalable web applications, along with working with data and AI to solve real-world problems.",
     profileImage: "/img/profile.jpg",
     aboutImage: "/img/about.jpg",
     contacts: {
@@ -14,82 +14,65 @@ const profileData = {
     skills: {
         languages: ['JavaScript', 'Python', 'TypeScript', 'Java', 'C++'],
         technologies: ['React', 'Node.js', 'Django', 'SQL', 'Tailwind', 'Docker', 'Postman', 'Git', 'Power BI', 'Tensorflow', 'MySQL', 'PostgreSQL', 'MongoDB']
-      },
+    },
     projects: [
         {
-            "title": "Fullstack Dynamic Sales Insights & Forecasting Platform",
-            "description": "A robust dashboard delivering real-time sales analytics and predictive insights powered by ARIMA models.",
-            "technologies": ["Python", "Django", "JS", "Chart.js", "Pandas", "ARIMA"],
-            "githubLink": "https://github.com/yourusername/sales-prevision-app",
-            "liveLink": "https://your-sales-prevision-app.vercel.app",
-            "previewImage": "/img/2.png"
+            title: "Fullstack Dynamic Sales Insights & Forecasting Platform",
+            description: "A robust dashboard delivering real-time sales analytics and predictive insights powered by ARIMA models.",
+            technologies: ["Python", "Django", "JS", "Chart.js", "Pandas", "ARIMA"],
+            githubLink: "https://github.com/Larsenvini/Django-Sales-Dashboard",
+            liveLink: "https://your-sales-prevision-app.vercel.app",
+            previewImage: "/img/2.png"
         },
-          
         {
-            title: "Website Acadêmico",
-            description: "Desenvolvimento de um site acadêmico completo com cadastro de usuários, autenticação e exibição de conteúdos personalizados.",
+            title: "Academic Website",
+            description: "Development of a complete academic website with user registration, authentication, and personalized content display.",
             technologies: ["TypeScript", "MongoDB", "Node.js"],
-            githubLink: "https://github.com/website-academico",
-            liveLink: "https://website-academico.com",
-            previewImage: "/path/to/website-academico-preview.jpg",
-          },
-
-          {
-            title: "Website Freelance para Academia de Futebol",
-            description: "Criação de um site profissional e responsivo, com foco em design moderno e funcionalidade, para uma academia de futebol italiana.",
+            githubLink: "https://github.com/Larsenvini/DotGeeks",
+            liveLink: "https://academic-website.com",
+            previewImage: "/img/dotgeeks.png",
+        },
+        {
+            title: "Freelance Website for a Soccer Academy",
+            description: "Creation of a professional, responsive website with a focus on modern design and functionality for an Italian soccer academy.",
             technologies: ["React", "Next.js", "Tailwind CSS"],
-            githubLink: "https://github.com/website-academia-futebol",
-            liveLink: "https://website-academia-futebol.com",
-            previewImage: "/path/to/website-academia-futebol-preview.jpg",
-          },
+            githubLink: "https://github.com/Larsenvini/PrivateProjects",
+            liveLink: "https://forza-seven.vercel.app/en",
+            previewImage: "/img/forza.png",
+        },
 
-          {
-            title: "Aplicação Financeira com Django e Machine Learning",
-            description: "Desenvolvimento de uma plataforma financeira que utiliza modelos preditivos para análise de gastos e receitas, com geração de relatórios dinâmicos.",
-            technologies: ["Django", "Python", "Pandas", "Scikit-learn"],
-            githubLink: "https://github.com/aplicacao-financeira-ml",
-            liveLink: "https://aplicacao-financeira-ml.com",
-            previewImage: "/path/to/aplicacao-financeira-preview.jpg",
-          },
-
-          {
-            title: "Sistema Especialista com Python, Prolog e Flask",
-            description: "Criação de um sistema especialista com interface web para tomada de decisão baseada em regras.",
+        {
+            title: "Expert System with Python, Prolog, and Flask",
+            description: "Creation of an expert system with a web interface for rule-based decision making.",
             technologies: ["Python", "Prolog", "Flask"],
-            githubLink: "https://github.com/sistema-especialista",
-            liveLink: "https://sistema-especialista.com",
-            previewImage: "/path/to/sistema-especialista-preview.jpg",
-          },
-
-          {
-            title: "Sistema empresarial de Entregas",
-            description: "Desenvolvimento de um aplicativo completo para gerenciamento de entregas, incluindo cadastro de pedidos, rastreamento e notificações.",
+            githubLink: "https://github.com/Larsenvini/Prolog_especialista",
+            liveLink: "https://expert-system.com",
+            previewImage: "/img/prolog.png",
+        },
+        {
+            title: "Delivery Management System",
+            description: "Development of a comprehensive application for managing deliveries, including order registration, tracking, and notifications.",
             technologies: ["Java"],
-            githubLink: "https://github.com/aplicativo-entregas",
-            liveLink: "https://aplicativo-entregas.com",
-            previewImage: "/path/to/aplicativo-entregas-preview.jpg",
-          },
-
-          {
-            title: "Sistema de Automação de E-mails",
-            description: "Criação de um sistema automatizado para envio e agendamento de e-mails com suporte a múltiplos provedores.",
+            githubLink: "https://github.com/Larsenvini/PrivateProjects",
+            liveLink: "https://delivery-management-app.com",
+            previewImage: "/img/java.pngg",
+        },
+        {
+            title: "Email Automation System",
+            description: "Creation of an automated system for sending and scheduling emails with support for multiple providers.",
             technologies: ["Python", "SMTP", "Schedule"],
-            githubLink: "https://github.com/sistema-automacao-emails",
-            liveLink: "https://sistema-automacao-emails.com",
-            previewImage: "/path/to/sistema-automacao-emails-preview.jpg",
-          },
-
-          {
-            title: "Portfólio Pessoal",
-            description: "Desenvolvimento de um portfólio interativo para exibir projetos e habilidades.",
+            githubLink: "https://github.com/Larsenvini/RacerMail-Mail-Automation",
+            liveLink: "https://email-automation-system.com",
+            previewImage: "/img/email.jpg",
+        },
+        {
+            title: "Personal Portfolio",
+            description: "Development of an interactive portfolio to showcase projects and skills.",
             technologies: ["React"],
-            githubLink: "https://github.com/portfolio-pessoal",
-            liveLink: "https://portfolio-pessoal.com",
-            previewImage: "/path/to/portfolio-pessoal-preview.jpg",
-          },
-          
-
-
+            githubLink: "https://github.com/Larsenvini/Portfolio",
+            liveLink: "https://larsenvini.pages.dev/",
+            previewImage: "/img/portfolio.png",
+        },
     ]
-  };
-  export default profileData;
+};
+export default profileData;
